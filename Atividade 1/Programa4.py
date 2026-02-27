023df4b48c195a467746ea0c45f507df3f70215f
mariosilva-junior-eng <mario_silva-junior@estudante.sesisenai.org.br>
2026-02-27 19:43:57 +0000

    update

---
+++ Atividade 1/Programa4.py
@@ -0,0 +1,10 @@
+nome = input("insira seu nome")
+nota1 = float(input("insira a primeira nota"))
+nota2 = float(input("insira a  segunda nota"))
+media = (nota1 + nota2) / 2
+print(" A média das notas é:", media)
+if media >= 7:
+    print("o aluno esta aprovado")
+else:
+     print("o aluno  esta reprovado")
+
