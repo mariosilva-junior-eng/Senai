49b1d138b0a5c38bdaa45b018665653014870c2d
mariosilva-junior-eng <mario_silva-junior@estudante.sesisenai.org.br>
2026-05-08 18:02:48 +0000

    update

--- Atividade 1/listadeexercicios/exercicio19.py
+++ Atividade 1/listadeexercicios/exercicio19.py
@@ -4,3 +4,6 @@ custo_chip = 4.0
 custo_alimento = 3.50
 
 gasto_por_frango = custo_chip + (2 * custo_alimento)
+gasto_total = qtd_frangos * gasto_por_frango
+print("A quantia de frangos na fazenda é: ", qtd_frangos)
+print("Para colocar a quantia desejada de aneis será preciso gastar:  ", gasto_total)
